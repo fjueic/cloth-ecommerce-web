b8c858794def48f0d0de18846b7563f30550ffd4
fjueic <rohitmalik987789@gmail.com>
2022-12-08 22:31:45 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Routes,Link } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.components";
 import ShopPage from "./pages/shop/shop.components";
-
+import "./App.css";
+import Header from "./components/header/header.component";
 function App() {
   return (
     <>
+    <Header/>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/shop" element={<ShopPage />} />
